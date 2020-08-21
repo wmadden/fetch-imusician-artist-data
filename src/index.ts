d9c2d7a0ce37cdeeef61797c25acb83d4c07b57b
Will Madden <w.a.madden@gmail.com>
2020-08-21 18:43:03 +0200

    Modify script to preserve input data
    - add options --inputFormat --outputFormat
    - support reading/writing CSV/JSON
    - write to output all data present in input (so Rachel can keep her initial data)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { SpotifyClient, ArtistObject, SimplifiedAlbumObject } from "./spotify";
 import * as yargs from "yargs";
+import { CastingContext } from "csv-parse";
+const csvParse = require("csv-parse");
+const csvStringify = require("csv-stringify");
 import { readFile, writeFile } from "fs";
 import { uniq, chunk, flatten } from "lodash";
+import { promisify } from "util";
 
 type RemoteIds = { spotify?: { id: string | null } };
 
-type ArtistData = {
+type InputRecord = {
+  originalInput: { [key: string]: string | number | Date | undefined };
   shop_artist_ids: RemoteIds | null;
 };
 
@@ -30,61 +35,108 @@ async function chunkRequests<InputElement, Result>(
   return flatten(result);
 }
 
-async function parseInputFile(file: string): Promise<ArtistData[]> {
+type CsvInputRow = {
+  shop_artist_ids: string;
+};
+async function parseInputCsv(file: string): Promise<InputRecord[]> {
+  const fileContent = await promisify(readFile)(file, "utf8");
+
+  function onRecord(record: CsvInputRow, context: CastingContext): InputRecord {
+    return {
+      originalInput: record,
+      shop_artist_ids: record.shop_artist_ids
+        ? JSON.parse(record.shop_artist_ids)
+        : null,
+    };
+  }
+
+  return await new Promise<InputRecord[]>((resolve, reject) => {
+    csvParse(
+      fileContent,
+      { columns: true, on_record: onRecord },
+      (err: any, data: InputRecord[]) => {
+        if (err) reject(err);
+        else resolve(data);
+      }
+    );
+  });
+}
+
+async function parseInputJson(file: string): Promise<InputRecord[]> {
   return new Promise((resolve, reject) => {
     readFile(file, "utf8", (err, data) => {
       if (err) reject(err);
       const parsedJson: { shop_artist_ids: string }[] = JSON.parse(data);
 
-      const artistDataArray = parsedJson.map((value) => {
+      const inputRecords = parsedJson.map((value) => {
         return {
-          ...value,
-          shop_artist_ids: JSON.parse(value.shop_artist_ids),
+          originalInput: value,
+          shop_artist_ids: value.shop_artist_ids
+            ? JSON.parse(value.shop_artist_ids)
+            : null,
         };
       });
 
-      resolve(artistDataArray);
+      resolve(inputRecords);
     });
   });
 }
 
-function escapeCsvCharacters(value: string | undefined | number): string {
-  if (typeof value === "number") return value.toString();
-  if (!value) return "";
+type OutputRecord = {
+  [key: string]: string | number | Date | undefined;
+  spotifyId: string | undefined;
+  artistName: string | undefined;
+  popularity: number | undefined;
+  followers: number | undefined;
+  latestAlbumName: string | undefined;
+  latestAlbumReleaseDate: string | undefined;
+};
 
-  return `"${value.replace(/(["\n])/g, "\\$1")}"`;
-}
-
-function writeOutput(
-  artists: ArtistObject[],
+function prepareOutputData(
+  inputRecords: InputRecord[],
+  spotifyIdToArtistObject: { [key: string]: ArtistObject },
   artistIdToLatestAlbumDict: {
     [key: string]: SimplifiedAlbumObject | undefined;
-  },
-  output: string
-) {
-  console.log(`Writing result to '${output}'...`);
+  }
+): OutputRecord[] {
+  return inputRecords.map(
+    ({ originalInput, shop_artist_ids }): OutputRecord => {
+      const spotifyId = shop_artist_ids?.spotify?.id || undefined;
+      const spotifyArtist = spotifyId
+        ? spotifyIdToArtistObject[spotifyId]
+        : undefined;
+      const latestAlbum = spotifyId
+        ? artistIdToLatestAlbumDict[spotifyId]
+        : undefined;
 
-  const csvData =
-    `spotifyId,name,popularity,followers,latest album name,latest album release date\n` +
-    artists
-      .map(({ id, name, popularity, followers: { total: followers } }) =>
-        [
-          id,
-          name,
-          popularity,
-          followers,
-          artistIdToLatestAlbumDict[id]?.name,
-          artistIdToLatestAlbumDict[id]?.release_date,
-        ]
-          .map(escapeCsvCharacters)
-          .join(",")
-      )
-      .join("\n");
+      return {
+        ...originalInput,
+        spotifyId: spotifyId,
+        artistName: spotifyArtist?.name,
+        popularity: spotifyArtist?.popularity,
+        followers: spotifyArtist?.followers.total,
+        latestAlbumName: latestAlbum?.name,
+        latestAlbumReleaseDate: latestAlbum?.release_date,
+      };
+    }
+  );
+}
 
-  writeFile(output, csvData, (err) => {
-    if (err) throw err;
-    console.log(`✅ Done!`);
+async function writeOutputCsv(
+  path: string,
+  outputRecords: OutputRecord[]
+): Promise<unknown> {
+  const outputString = await promisify(csvStringify)(outputRecords, {
+    header: true,
   });
+  return promisify(writeFile)(path, outputString);
+}
+
+function writeOutputJson(
+  path: string,
+  outputRecords: OutputRecord[]
+): Promise<unknown> {
+  return promisify(writeFile)(path, JSON.stringify(outputRecords));
 }
 
 async function main({
@@ -92,16 +144,15 @@ async function main({
   clientSecret,
   input,
   output,
-}: {
-  clientId: string;
-  clientSecret: string;
-  input: string;
-  output: string;
-}) {
-  const artistDataArray = await parseInputFile(input);
+  inputFormat,
+  outputFormat,
+}: Options) {
+  const inputRecords = await (inputFormat === "json"
+    ? parseInputJson(input)
+    : parseInputCsv(input));
 
   let spotifyIds: string[] = [];
-  artistDataArray.forEach((artistData) => {
+  inputRecords.forEach((artistData) => {
     if (artistData.shop_artist_ids?.spotify?.id) {
       spotifyIds.push(artistData.shop_artist_ids.spotify.id);
     }
@@ -111,52 +162,98 @@ async function main({
   const spotifyClient = new SpotifyClient();
   await spotifyClient.authenticate(clientId, clientSecret);
 
-  let artists: ArtistObject[] = [];
-  let artistIdToLatestAlbumDict: {
+  const spotifyIdToArtistObjectDict: { [key: string]: ArtistObject } = {};
+  const artistIdToLatestAlbumDict: {
     [key: string]: SimplifiedAlbumObject | undefined;
   } = {};
+  let outputRecords: OutputRecord[] = [];
 
   try {
     const spotifyIdsInChunksOf50 = chunk(spotifyIds, 50);
 
-    const chunkedArtists = await chunkRequests(
-      spotifyIdsInChunksOf50,
-      async (idsChunk, index) => {
+    await Promise.all([
+      chunkRequests(spotifyIdsInChunksOf50, async (idsChunk, index) => {
         console.log(
           `Fetching artists ${index * 50} - ${index * 50 + 50} / ${
             spotifyIds.length
           }...`
         );
-        return await spotifyClient.getArtists(idsChunk);
-      }
-    );
-    artists = flatten(chunkedArtists);
+        const artistsChunk = await spotifyClient.getArtists(idsChunk);
+        artistsChunk.forEach(
+          (artist) => (spotifyIdToArtistObjectDict[artist.id] = artist)
+        );
+      }),
+      chunkRequests(spotifyIds, async (spotifyId, i) => {
+        console.log(
+          `Fetching latest album for artist ${i} / ${spotifyIds.length}`
+        );
+        artistIdToLatestAlbumDict[
+          spotifyId
+        ] = await spotifyClient.getLatestAlbum(spotifyId);
+      }),
+    ]);
 
-    await chunkRequests(artists, async (artist, i) => {
-      console.log(`Fetching latest album for artist ${i} / ${artists.length}`);
-      artistIdToLatestAlbumDict[artist.id] = await spotifyClient.getLatestAlbum(
-        artist.id
-      );
-    });
+    outputRecords = prepareOutputData(
+      inputRecords,
+      spotifyIdToArtistObjectDict,
+      artistIdToLatestAlbumDict
+    );
   } finally {
-    writeOutput(artists, artistIdToLatestAlbumDict, output);
+    console.log(`Writing result to '${output}' as ${outputFormat}...`);
+
+    if (outputFormat === "json") {
+      await writeOutputJson(output, outputRecords);
+    } else {
+      await writeOutputCsv(output, outputRecords);
+    }
+    console.log(`✅ Done!`);
   }
 }
 
+type Options = {
+  clientId: string;
+  clientSecret: string;
+  input: string;
+  output: string;
+  inputFormat: "json" | "csv";
+  outputFormat: "json" | "csv";
+};
+
 const options = yargs
+  .options({
+    clientId: {
+      required: true,
+      description: "Client ID of your Spotify app",
+      type: "string",
+    },
+    clientSecret: {
+      required: true,
+      description: "Client secret of your Spotify app",
+      type: "string",
+    },
+    input: {
+      description: "File containing artist data",
+      default: "input.json",
+    },
+    inputFormat: {
+      description: "Input file format",
+      choices: ["json", "csv"],
+      default: "json",
+    },
+    outputFormat: {
+      description: "Output file format",
+      choices: ["json", "csv"],
+      default: "csv",
+    },
+    output: {
+      description: "File to write result data to",
+      default: "out.csv",
+    },
+  })
   .usage(
     `Usage: $0 --clientId=ID --clientSecret=SECRET --input=FILE --output=FILE
   
   See README for how to obtain Spotify credentials`
-  )
-  .describe("clientId", "Client ID of your Spotify app")
-  .string("clientId")
-  .describe("clientSecret", "Client secret of your Spotify app")
-  .string("clientSecret")
-  .describe("input", "JSON file containing artist data")
-  .default("input", "input.json")
-  .describe("output", "File to write result data to")
-  .default("output", "out.csv")
-  .demandOption(["clientId", "clientSecret"]).argv;
+  ).argv as Options;
 
 main(options);
